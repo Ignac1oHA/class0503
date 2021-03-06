c2e5b4020ccb6a87752293399b9e654df71d197d
ihenriquez <ihenriquez86@gmail.com>
2021-03-06 10:30:12 -0300

    Slice Example

---
+++ Index.py
@@ -0,0 +1,8 @@
+hi = "Hello"
+
+len(hi)
+
+print(len(hi))
+
+hi[::-1]
+print("hi")
---
+++ StringsandFiles.py
@@ -0,0 +1,22 @@
+hi="hello"
+print(hi[2])
+print("hello2"[2])
+
+if hi[2] == hi[3]:
+    print("This is true because we have double l")
+
+greeting = "hi " + "how " + "are " + "you " + "?"
+print(greeting)
+
+# Multiplication
+
+print("bye " * 2)
+
+if "hi" > "HI":
+    print("hi > HI")
+else:
+    print("hi" < "HI")
+print("1ello" > "hello")
+print("helloooo" > "hello")
+print("ana" in "banana")
+
